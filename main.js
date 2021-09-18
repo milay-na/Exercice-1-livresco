de743ce0a5f01638a3e0f0b933c8691ed4cca3d8
Milena du Manoir <milenadumanoir@gmail.com>
2021-09-18 22:34:15 +0200

    Clean the redundance
    Erased the fibagain function, and made a fib function that works when you loop it!
    Did the same for display and displayNext!!

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-function fib() {
-    var a = BigInt(0);
-    var b = BigInt(1);
-    var fiblist = [];
-    fiblist[0] = a;
-    fiblist[1] = b;
+var first = BigInt(0);
+var second = BigInt(1);
+var num = 0;
 
-    console.log(a.toString());
-    console.log(b.toString());
+function fib() {
+    var a = first;
+    var b = second;
+    var fiblist = [];
+
+    fiblist[0] = first;
+    fiblist[1] = second;
 
     for (let i = 0; i < 98; i = i + 1) {
         var next = a + b;
@@ -14,69 +16,28 @@ function fib() {
         b = next; 
         fiblist[i + 2] = next;
         console.log(next.toString());  
-
-    }
-
-    return fiblist;
-}
-var fiblist = fib()
-var avantDernier = fiblist[98];
-var dernier = fiblist[99];
-var num = 0;
-
-function fibagain() {
-    var fiblist = fib();
-    var c = avantDernier;
-    var d = dernier;
-    var fiblist = [];
-    fiblist[0] = c + d;
-    fiblist[1] = d + fiblist[0];
-
-    console.log(c.toString());
-    console.log(d.toString());
-
-    c = fiblist[0];
-    d = fiblist[1];
-    for (let i = 0; i < 98; i = i + 1) {
-        var next = c + d;
-        c = d;
-        d = next; 
-        fiblist[i + 2] = next;
-        console.log(next.toString());  
     }
     
-    avantDernier = fiblist [98];
-    dernier = fiblist [99];
+    first = fiblist[98] + fiblist[99] ;
+    second = fiblist[99] + first;
 
     return fiblist;
-}
-
-function displayFib() {
-    var element = document.getElementById("list");
-    var fiblist = fib();
-    
-    for (let i = 0; i < 100; i = i + 1) {
-        var item = document.createElement("p");
-        var text = document.createTextNode("F(" + (num++) + ") = " + fiblist[i].toString());
-        item.appendChild(text);
-        element.appendChild(item);
-    }
 }
 
 function displayNextFib() { 
     var element = document.getElementById("list");
-    var fiblist = fibagain();
+    var fiblist = fib();
+
     element.innerHTML = "";
+
     for (let i = 0; i < 100; i = i + 1) {
         var item = document.createElement("p");
-        var text = document.createTextNode("F(" + (num++) + ") = " + fiblist[i].toString());
+        var text = document.createTextNode("F(" + num + ") = " + fiblist[i].toString());
         item.appendChild(text);
         element.appendChild(item);
+        num = num + 1;
     }
-
-
-
 }
 
 
-displayFib();
+displayNextFib();
