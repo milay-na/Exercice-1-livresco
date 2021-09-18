bb71dd785d02a7ff4aa0e6d4b3aec11e98f235be
Milena du Manoir <milenadumanoir@gmail.com>
2021-09-18 21:28:45 +0200

    add styles.css, add next loops to compute fibonacci sequence(main.js), add button
    styles.css is used for the background, button and to center the list generated.
    main.js contains new functions (fibagain() and displayNext() that enable the computation of fibonacci sequence.
    html file now contains onclick button using displayNext function.
    The loop works, but the code is uuh.

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 function fib() {
     var a = BigInt(0);
     var b = BigInt(1);
@@ -21,17 +19,64 @@ function fib() {
 
     return fiblist;
 }
+var fiblist = fib()
+var avantDernier = fiblist[98];
+var dernier = fiblist[99];
+var num = 0;
+
+function fibagain() {
+    var fiblist = fib();
+    var c = avantDernier;
+    var d = dernier;
+    var fiblist = [];
+    fiblist[0] = c + d;
+    fiblist[1] = d + fiblist[0];
+
+    console.log(c.toString());
+    console.log(d.toString());
+
+    c = fiblist[0];
+    d = fiblist[1];
+    for (let i = 0; i < 98; i = i + 1) {
+        var next = c + d;
+        c = d;
+        d = next; 
+        fiblist[i + 2] = next;
+        console.log(next.toString());  
+    }
+    
+    avantDernier = fiblist [98];
+    dernier = fiblist [99];
+
+    return fiblist;
+}
 
 function displayFib() {
     var element = document.getElementById("list");
-    var fiblist = fib()
+    var fiblist = fib();
     
     for (let i = 0; i < 100; i = i + 1) {
         var item = document.createElement("p");
-        var text = document.createTextNode(fiblist[i].toString());
+        var text = document.createTextNode("F(" + (num++) + ") = " + fiblist[i].toString());
         item.appendChild(text);
         element.appendChild(item);
     }
 }
+
+function displayNextFib() { 
+    var element = document.getElementById("list");
+    var fiblist = fibagain();
+    element.replaceChildren();
+    for (let i = 0; i < 100; i = i + 1) {
+        var item = document.createElement("p");
+        var text = document.createTextNode("F(" + (num++) + ") = " + fiblist[i].toString());
+        item.appendChild(text);
+        element.appendChild(item);
+    }
+
+
+
+}
+
 
 displayFib();
